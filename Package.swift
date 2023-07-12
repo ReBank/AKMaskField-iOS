18693b7fbd20fe7f16bd12da5cb28b6a5fada9de
Giorgi Vashakidze <gvashakidze@kaltura.com>
2023-07-12 12:05:38 +0400

    ...

--- Package.swift
+++ Package.swift
@@ -4,22 +4,22 @@
 import PackageDescription
 
 let package = Package(
-    name: "AKMaskField",
+    name: "AKMaskField-iOS",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "AKMaskField",
-            targets: ["AKMaskField"]),
+            name: "AKMaskField-iOS",
+            targets: ["AKMaskField-iOS"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "AKMaskField",
-            dependencies: ["AKMaskField"]
+            name: "AKMaskField-iOS",
+            path: ["Sources"]
         ),
         .testTarget(
-            name: "AKMaskFieldTests",
-            dependencies: ["AKMaskField"]),
+            name: "AKMaskField-iOSTests",
+            dependencies: ["AKMaskField-iOS"]),
     ]
 )
